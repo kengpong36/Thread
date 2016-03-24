35db97f584d4f34fa8b025a47a7117b45a457c8a
Keng <kengpong36@gmail.com>
2016-03-24 13:57:38 +0700

    firt

---
+++ test/thread/ThreadGUITest.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thread;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrator
+ */
+public class ThreadGUITest {
+    
+    public ThreadGUITest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of actionPerformed method, of class ThreadGUI.
+     */
+    @Test
+    public void testActionPerformed() {
+        System.out.println("actionPerformed");
+        ActionEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.actionPerformed(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mouseClicked method, of class ThreadGUI.
+     */
+    @Test
+    public void testMouseClicked() {
+        System.out.println("mouseClicked");
+        MouseEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.mouseClicked(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mouseEntered method, of class ThreadGUI.
+     */
+    @Test
+    public void testMouseEntered() {
+        System.out.println("mouseEntered");
+        MouseEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.mouseEntered(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mouseExited method, of class ThreadGUI.
+     */
+    @Test
+    public void testMouseExited() {
+        System.out.println("mouseExited");
+        MouseEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.mouseExited(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mousePressed method, of class ThreadGUI.
+     */
+    @Test
+    public void testMousePressed() {
+        System.out.println("mousePressed");
+        MouseEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.mousePressed(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of mouseReleased method, of class ThreadGUI.
+     */
+    @Test
+    public void testMouseReleased() {
+        System.out.println("mouseReleased");
+        MouseEvent evt = null;
+        ThreadGUI instance = new ThreadGUI();
+        instance.mouseReleased(evt);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class ThreadGUI.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ThreadGUI.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
